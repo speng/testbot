b0bf88a1dba70bfb20f2da74103b041fee4f3549
Siew Peng <Siew Peng>
2017-01-05 13:38:18 +0800

    integrate wit-ai

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('request');
 var app = express();
+var WIT_TOKEN = '4UWV4QHZ2KWYUMUYO7VRPUVLFIA5YB7M';
+const {Wit, log} = require('node-wit');
+
+const accessToken = '4UWV4QHZ2KWYUMUYO7VRPUVLFIA5YB7M';
+let Wit = null;
+let interactive = null;
+try {
+  // if running from repo
+  Wit = require('../').Wit;
+  interactive = require('../').interactive;
+} catch (e) {
+  Wit = require('node-wit').Wit;
+  interactive = require('node-wit').interactive;
+}
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
@@ -99,3 +113,38 @@ function kittenMessage(recipientId, text) {
     return false;
     
 };
+
+//WIT.AI
+const firstEntityValue = (entities, entity) => {
+  const val = entities && entities[entity] &&
+    Array.isArray(entities[entity]) &&
+    entities[entity].length > 0 &&
+    entities[entity][0].value
+  ;
+  if (!val) {
+    return null;
+  }
+  return typeof val === 'object' ? val.value : val;
+};
+
+const actions = {
+  send(request, response) {
+    const {sessionId, context, entities} = request;
+    const {text, quickreplies} = response;
+    console.log('sending...', JSON.stringify(response));
+  },
+  getForecast({context, entities}) {
+    var location = firstEntityValue(entities, 'location');
+    if (location) {
+      context.forecast = 'sunny in ' + location; // we should call a weather API here
+      delete context.missingLocation;
+    } else {
+      context.missingLocation = true;
+      delete context.forecast;
+    }
+    return context;
+  },
+};
+
+const client = new Wit({accessToken, actions});
+interactive(client);
