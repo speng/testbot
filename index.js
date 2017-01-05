40da91a08c7c172c66b972714c866d8908f7d6d9
Siew Peng <Siew Peng>
2017-01-05 14:54:31 +0800

    fix bug

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.get('/webhook', function (req, res) {
 // handler receiving messages
 app.post('/webhook', function (req, res) {
     var events = req.body.entry[0].messaging;
-    for (i = 0; i < events.length; i++) {
+    for (int i = 0; i < events.length; i++) {
         var event = events[i];
         if (event.message && event.message.text) {
             if (!kittenMessage(event.sender.id, event.message.text)) {
